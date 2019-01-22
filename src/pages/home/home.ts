690c60acd2c1c410fb48bbd315f0f019fef5905d
Giovanni Pires da Silva <giovannipds@gmail.com>
2019-01-22 15:55:43 -0200

    Add necessary home page controller properties

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -8,6 +8,30 @@ import { ImagesProvider } from '../../providers/images/images';
 })
 export class HomePage {
 
+  /**
+   * @name image
+   * @type String
+   * @public
+   * @description Will store the selected image file data (in the form of a base64 data URI)
+   */
+  public image : string;
+
+  /**
+   * @name isSelected
+   * @type Boolean
+   * @public
+   * @description Used to switch DOM elements on/off depending on whether an image has been selected
+   */
+  public isSelected : boolean = false;
+
+  /**
+   * @name _SUFFIX
+   * @type String
+   * @private
+   * @description Will store the selected image's MimeType
+   */
+  private _SUFFIX : string;
+
   constructor(public navCtrl: NavController) {
 
   }
