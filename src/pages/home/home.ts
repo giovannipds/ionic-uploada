1575f6c084e4cbfc9aaa3ce048fbf8c2a166c104
Giovanni Pires da Silva <giovannipds@gmail.com>
2019-01-22 16:02:44 -0200

    Write selectFileUpload method at home page controller

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -37,4 +37,45 @@ export class HomePage {
               private _IMAGES : ImagesProvider)
   { }
 
+  /**
+   * @public
+   * @method selectFileToUpload
+   * @param event {any} The DOM event that we are capturing from the File input field
+   * @description Handles the selection of image files from the user's computer,
+   *              validates they are of the correct file type and displays the
+   *              selected image in the component template along with an upload
+   *              button
+   * @return {none}
+   */
+  selectFileToUpload(event) : void
+  {
+    this._IMAGES
+    .handleImageSelection(event)
+    .subscribe((res) =>
+    {
+      // Retrieve the file type from the base64 data URI string
+      this._SUFFIX = res.split(':')[1].split('/')[1].split(";")[0];
+
+      // Do we have correct file type?
+      if(this._IMAGES.isCorrectFileType(this._SUFFIX))
+      {
+        // Hide the file input field, display the image in the component template
+        // and display an upload button
+        this.isSelected = true
+        this.image      = res;
+      }
+
+      // If we don't alert the user
+      else
+      {
+        this.displayAlert('You need to select an image file with once of the following types: jpg, gif or png');
+      }
+    },
+    (error) =>
+    {
+      console.dir(error);
+      this.displayAlert(error.message);
+    });
+  }
+
 }
