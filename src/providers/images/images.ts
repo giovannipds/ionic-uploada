3735cf2da0f54b50498d4ffa472fe59288014530
Giovanni Pires da Silva <giovannipds@gmail.com>
2019-01-22 15:28:45 -0200

    Write handleImageSelection method

--- src/providers/images/images.ts
+++ src/providers/images/images.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs/Observable';
 
 /*
   Generated class for the ImagesProvider provider.
@@ -28,6 +29,30 @@ export class ImagesProvider {
   
   constructor(public http: HttpClient) {
     console.log('Hello ImagesProvider Provider');
+  }
+  
+  /**
+   * @public
+   * @method handleImageSelection
+   * @param event {any} The DOM event that we are capturing from the File input field
+   * @description Uses the FileReader API to capture the input field event, retrieve
+   *              the selected image and return that as a base64 data URL courtesy of
+   *              an Observable
+   * @return {Observable}
+   */
+  handleImageSelection(event : any) : Observable<any>
+  {
+    let file : any = event.target.files[0];
+
+    this._READER.readAsDataURL(file);
+    return Observable.create((observer) => 
+    {
+      this._READER.onloadend = () =>
+      {
+        observer.next(this._READER.result);
+        observer.complete();
+      }
+    });
   }
 
 }
